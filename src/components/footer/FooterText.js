3bbf6c9a546354be364c86491fd8601e0de42d42
tybens <tb19@princeton.edu>
2022-08-29 11:29:06 -0700

    update portolio button to be text '

--- src/components/footer/FooterText.js
+++ src/components/footer/FooterText.js
@@ -1,8 +1,6 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "components/theme/ThemeProvider";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { HeartIcon, HeartIconFilled } from "components/content/ResumeButton";
-import { Link, Tooltip, IconButton, Zoom } from "@material-ui/core";
+import { Link, Tooltip, Zoom, Button } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   iconButton: {
@@ -10,7 +8,6 @@ const useStyles = makeStyles((theme) => ({
     bottom: theme.spacing(6),
     left: theme.spacing(6),
     height: "2.5rem",
-    width: "2.5rem",
   },
   icon: {
     fontSize: "1.25rem",
@@ -18,26 +15,21 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const FooterText = () => {
-  const { theme } = useContext(ThemeContext);
 
   const classes = useStyles();
   const handleClickPortfolio = () => {};
   return (
-    <Link
-      href={"/portfolio"}
-      key="/portfolio"
-      underline="none"
-      color="inherit"
-    >
+    <Link href={"/portfolio"} key="/portfolio" underline="none" color="inherit">
       <Tooltip title={"Portfolio"} placement="right" TransitionComponent={Zoom}>
-        <IconButton
+        <Button
+          variant="text"
           color="inherit"
           onClick={handleClickPortfolio}
           aria-label={"Go to resume"}
           className={classes.iconButton}
         >
-          {theme === "light" ? <HeartIcon /> : <HeartIconFilled />}
-        </IconButton>
+          Portfolio
+        </Button>
       </Tooltip>
     </Link>
   );
